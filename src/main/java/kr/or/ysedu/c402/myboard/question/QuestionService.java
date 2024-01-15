4bfd98872265681b6741f2cee171cd4b651c2703
leeseulkwak <leesel1690@gmail.com>
2024-01-15 16:23:19 +0900

    2-09

---
+++ src/main/java/kr/or/ysedu/c402/myboard/question/QuestionService.java
@@ -0,0 +1,19 @@
+package kr.or.ysedu.c402.myboard.question;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class QuestionService {
+
+    private final QuestionRepository questionRepository;
+
+    public List<Question> getList() {
+        return this.questionRepository.findAll();
+    }
+
+}
