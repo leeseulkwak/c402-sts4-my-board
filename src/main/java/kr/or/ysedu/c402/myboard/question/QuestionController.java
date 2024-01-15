65dc9b92e78638892b675de9859b3b3d57858244
leeseulkwak <leesel1690@gmail.com>
2024-01-15 17:37:48 +0900

    2-11

--- src/main/java/kr/or/ysedu/c402/myboard/question/QuestionController.java
+++ src/main/java/kr/or/ysedu/c402/myboard/question/QuestionController.java
@@ -6,10 +6,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import lombok.RequiredArgsConstructor;
-
+@RequestMapping("/question")
 @RequiredArgsConstructor
 @Controller
 public class QuestionController {
@@ -17,7 +18,7 @@ public class QuestionController {
 //	private final QuestionRepository questionRepository;
 	private final QuestionService questionService;
 	
-	@GetMapping("/question/list")
+	@GetMapping("/list")
 	public String list(Model model) {
 		List<Question> questionList=this.questionService.getList();
 		model.addAttribute("questionList", questionList);
@@ -29,7 +30,7 @@ public class QuestionController {
 		return "redirect:/question/list";
 	}
 	
-	@GetMapping(value = "/question/detail/{id}")
+	@GetMapping(value = "/detail/{id}")
     public String detail(Model model, @PathVariable("id") Integer id) {
 		Question question=this.questionService.getQuestion(id);
 		model.addAttribute("question", question);
