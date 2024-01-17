882daead6f2c89153698dfc386d80300875060b9
leeseulkwak <leesel1690@gmail.com>
2024-01-17 17:21:36 +0900

    3-11 앵커 기능 추가하기

--- src/main/java/kr/or/ysedu/c402/myboard/answer/AnswerController.java
+++ src/main/java/kr/or/ysedu/c402/myboard/answer/AnswerController.java
@@ -37,8 +37,8 @@ public class AnswerController {
             model.addAttribute("question", question);
             return "question_detail";
         }
-		this.answerService.create(question, answerForm.getContent(), siteUser);	//답변을 저장
-		return String.format("redirect:/question/detail/%s", id);
+		Answer answer=this.answerService.create(question, answerForm.getContent(), siteUser);
+		return String.format("redirect:/question/detail/%s#answer_%s", answer.getQuestion().getId(), answer.getId());
 	}
 	
 	 @PreAuthorize("isAuthenticated()")
@@ -63,7 +63,7 @@ public class AnswerController {
 	            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정권한이 없습니다.");
 	        }
 	        this.answerService.modify(answer, answerForm.getContent());
-	        return String.format("redirect:/question/detail/%s", answer.getQuestion().getId());
+	        return String.format("redirect:/question/detail/%s#answer_%s", answer.getQuestion().getId(), answer.getId());
 	    }
 	  
 	  @PreAuthorize("isAuthenticated()")
@@ -83,6 +83,6 @@ public class AnswerController {
 	        Answer answer = this.answerService.getAnswer(id);
 	        SiteUser siteUser = this.userService.getUser(principal.getName());
 	        this.answerService.vote(answer, siteUser);
-	        return String.format("redirect:/question/detail/%s", answer.getQuestion().getId());
+	        return String.format("redirect:/question/detail/%s#answer_%s", answer.getQuestion().getId(), answer.getId());
 	    }
 }
